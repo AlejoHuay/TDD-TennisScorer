20f685a1d4e33dd3fa5cd72d62362ced906a77cc
Alejandro Alvarez <alvarez.h.alejo@gmail.com>
2025-08-31 17:49:15 -0400

    refactoring: ahora se encapsula en objetos la funcionalidad del scorer

--- src/tennisScorer.js
+++ src/tennisScorer.js
@@ -1,5 +1,15 @@
-function showScore() {
-  return "Love - Love";
+class TennisScorer {
+  constructor(){
+    this.player1Points = 0;
+  }
+
+  showScore(){
+    return "Love - Love";
+  }
+
+  player1Scores() {
+    this.player1Points++;
+  }
 }
 
-export default showScore;
+export default TennisScorer;
--- src/tennisScorer.spec.js
+++ src/tennisScorer.spec.js
@@ -1,8 +1,15 @@
-import showScore from "./tennisScorer.js";
+import TennisScorer from "./tennisScorer.js";
+
+//Para un ciclo de TDD primero modificamos  la prueba it anterior (copiamos una nueva) para que de error (rojo)
+// luego modificamos la funcion para que la sig prueba ya de verde
+// si es nescesario refactorizamos antes de hacer commit
 
 describe("TennisScorer", () => {
-  //Empieza sin puntos     --> "Love - Love"
-  it("game empieza con ambos en cero", () => {
-    expect(showScore()).toEqual("Love - Love");
+    
+  // Empeiza sin puntos ---> "Love - Love"
+  it("game empieza con ambos jugadores en cero", () => {
+    const scorer = new TennisScorer();
+    expect(scorer.showScore()).toEqual("Love - Love");
   });
+
 });
